5675a5deb77145c065e08ba7476471d11f297167
Gloridust <Gloridust@gmail.com>
2024-02-20 19:03:18 +0800

    Update test.py

--- test.py
+++ test.py
@@ -10,11 +10,12 @@ false_count = 0
 # Repeat the detection process 10 times
 for _ in range(5):
     is_spam = detect_spam(sms_content)
-    if is_spam:
+    if is_spam == 'True':
         true_count += 1
-    else:
+    elif is_spam == 'False':
         false_count += 1
 
 # Print the counts
+print(is_spam)
 print(f"Number of times classified as spam: {true_count}")
 print(f"Number of times classified as not spam: {false_count}")
