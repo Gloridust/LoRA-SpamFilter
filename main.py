f0a576ea2779abc2dc41dd05130f8c6694271d2c
Gloridust <Gloridust@gmail.com>
2024-02-20 15:45:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
 from llama_cpp import Llama
 import json
 
-# 初始化模型
+# Initialize
 llm = Llama(
   model_path="./phi-2.Q4_K_M.gguf",
-  n_ctx=512,
-  n_threads=1,
+  n_ctx=1024,
+  n_threads=4,
   n_gpu_layers=0
 )
 
-# 用户输入短信内容
+# User input SMS content
 user_input = input("Input your SMS：")
 
-# 构建prompt
+# Build prompt
 prompt = f"Assess whether the following message is spam. Verification code messages should not be considered spam. Output 'True' for spam, 'False' otherwise. Only respond with 'True' or 'False'. Exclude verification codes from spam. Message: '{user_input}'\nOutput:"
 
-
-# 执行模型推理
+# Execute model inference
 output = llm(prompt, max_tokens=512,echo=False)
 
-# 解析模型输出
+# Parse model output
 try:
-    is_spam = output['choices'][0]['text'].strip()  # 根据实际输出结构调整
-    print(f"Spam: {is_spam}")  # 输出判断结果
+    is_spam = output['choices'][0]['text'].strip()  # Adjust based on the actual output structure
+    print(f"Spam: {is_spam}")  # Output judgment result
 except Exception as e:
     print(f"Error parsing model output: {e}")
-
