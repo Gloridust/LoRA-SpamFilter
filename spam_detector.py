b96f911b3d0dfcde7288dc37f5db2dcc52b74f06
Gloridust <Gloridust@gmail.com>
2024-02-28 23:36:59 +0800

    Update spam_detector.py

--- spam_detector.py
+++ spam_detector.py
@@ -1,22 +1,15 @@
-from langchain.llms import Ollama
-from langchain.callbacks.manager import CallbackManager
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallHandler
+import ollama
 
 ollama_host = "localhost"
 ollama_port = 11434
 ollama_model = "gemma:7b"
 
 def detect_spam(user_input):
-    
     prompt = f"Assess whether the following message is spam. Verification code messages should not be considered spam. Output 'True' for spam, 'False' otherwise. Only respond with 'True' or 'False' in one word. Message: '{user_input}'"
-    llm = Ollama(base_url=f"http://{ollama_host}:{ollama_port}", 
-        model=ollama_model,
-        callback_manager=CallbackManager([StreamingStdOutCallHandler()])
-        )
     # Execute model inference
-    output = llm(prompt, max_tokens=128, echo=False)
+    output = ollama.generate(model='gemma:7b', prompt=prompt)
     # Parse model output
-    is_spam = output['choices'][0]['text'].strip()  # Adjust based on the actual output structure
+    is_spam = output['message']['content']
     if "True" in is_spam:
         is_spam= True
         return is_spam
